0015bc67e9f54540d6d649e977b8f7f150d2c108
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2023-02-22 10:33:50 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import allReducers from "./reducers";
+
+// Store => Globalized State
+// pass in reducer
+
+// Action => Describe what you want to do
+// Increment
+// Function that returns object
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+store.subscribe(() => console.log(store.getState()));
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "SIGN_IN" });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
